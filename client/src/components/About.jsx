36a4654fd31e57118316c8abd18810656025eda2
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2020-07-30 21:20:35 +0100

    About Page updated

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import aboutImg from "../../public/aboutImg.svg";
 function About(){
     return(
-        <div className="">
-            <div className= "company-card">
-                <img className="cardImg" src={aboutImg} alt=""/>
-                <div className="container">
-                    <p>A blog for gossipers, by gossipers, to gossipers</p>
-                </div>
+        <div className= "aboutBody">
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <h1 className="aboutTatafo">Tatafo Blog</h1>
+        <div className="about">
+                <img className="aboutImg" src={aboutImg} alt=""/>
+                <div className="aboutText">
+                    <p>"A blog for gossipers, by gossipers, to gossipers"</p>
             </div>
         </div>
+        <div className="seventh-division aboutSeven">
+  <a className="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
+ <a className="footer-link" href="https://twitter.com/">Twitter</a>
+ <a className="footer-link" href="http://uyi.com">Website</a>
+ <p className="last">© 2020 <span class="last-last">Omoruyi Ohuoba.</span> All Rights Reserved.</p>
+</div>
+
+    
+        </div>
+        
     )
 }
 
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,10 +3,21 @@ import contactImg from "../../public/contactImg.svg";
 
 function Contact(){
     return(
-        <div className>
-            <div>
+        <div className="contactBody">
+            <div className="contactTop">
+            <img className="contactImg" src={contactImg} alt=""/>
+            <div className="contactContent">
+            <p>tatafo</p>
 
             </div>
+            </div>
+        
+            <div className="seventh-division contactSeven">
+  <a className="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
+ <a className="footer-link" href="https://twitter.com/">Twitter</a>
+ <a className="footer-link" href="http://uyi.com">Website</a>
+ <p className="last">© 2020 <span class="last-last">Omoruyi Ohuoba.</span> All Rights Reserved.</p>
+</div>
         </div>
     )
 }
