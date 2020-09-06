6926dc1a9dda484b65a462942ff6bbfe83513485
OmoruyiOhuoba <omoruyiohuoba@yahoo.com>
2020-09-06 23:54:13 +0100

    Deployment update

---
+++ client/build/precache-manifest.9947becc37da5f988d591e254bc0032f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35956d7a55b4fb3146f1b2bacee51c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b3fe83b396439500d1b",
+    "url": "/static/css/main.312d3a65.chunk.css"
+  },
+  {
+    "revision": "ae811f150846a617d284",
+    "url": "/static/js/2.03fff352.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03fff352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3fe83b396439500d1b",
+    "url": "/static/js/main.6bb8f459.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "cfcd5d365814e53357819d80b7fe97f4",
+    "url": "/static/media/Background.eeeeab6c.jpeg"
+  },
+  {
+    "revision": "09c0263dc7634794f9902f748db10d3a",
+    "url": "/static/media/Newspaper.88c05f4b.jpg"
+  },
+  {
+    "revision": "3b8554fb3555f407455dda9d8fdbb322",
+    "url": "/static/media/bbc-2.3b8554fb.svg"
+  },
+  {
+    "revision": "8c9cee4f88a43efb2cfb47ee4fb63bd0",
+    "url": "/static/media/cnn-1.8c9cee4f.svg"
+  },
+  {
+    "revision": "fa8b327865b79a1d182ed648170bd06d",
+    "url": "/static/media/fashion.fa8b3278.svg"
+  },
+  {
+    "revision": "0383e1b59586740de8367d5e0c0f48ce",
+    "url": "/static/media/gossip.0383e1b5.svg"
+  },
+  {
+    "revision": "72c1338e0934456dbac619658327661e",
+    "url": "/static/media/nasa-6.72c1338e.svg"
+  },
+  {
+    "revision": "e18dc65a6ac1d0c4be06eea31bef212b",
+    "url": "/static/media/news.e18dc65a.svg"
+  }
+]);
